bcd257a9b7b73f9bf3a20041277f6c8228b2f865
Александр <sashaneiv01@yandex.ru>
2021-11-23 00:17:31 +0300

    ДЗ 4 вариант1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,25 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// Ожидание выполнения ДЗ
+        // задача 1, 2
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        //задача 3
+        int year = 3000;
+        int year1 = year & 4;
+        if (year1 == 0) {
+            System.out.println(year + " год является високосным");
+        }  else {
+            System.out.println(year + " год не является високосным"); //доработать: не выводит каждый 400ый год согласно условию
+        }
     }
 }
