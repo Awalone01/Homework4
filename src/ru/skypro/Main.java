739d99c86692a2ae36572a8b90519998e5ce2b17
Александр <sashaneiv01@yandex.ru>
2021-11-23 22:07:54 +0300

    ДЗ 4 попытка  №2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,75 +1,82 @@
 package ru.skypro;
 
 public class Main {
-
     public static void main(String[] args) {
         // задача 1, 2
-        int clientOS = 1;
-        int clientDeviceYear = 2015;
+        int clientOS = 0;
+        int clientDeviceYear = 2011;
 
-        if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear <= 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientOS == 0) {
+                if (clientDeviceYear <= 2015)
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOS == 1) {
+                if (clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
         }
-        //задача 3
-        int year = 1000;
-        int yearOne = year & 4;
-        int yearTwo = year & 400;
+            //задача 3
+            int year = 1300;
+            boolean everyHoundredYear = (year % 100 == 0);
 
-        if (yearOne == 0 && yearTwo == 0) {
-            System.out.println(year + " год является високосным");
-        }  else {
-            System.out.println(year + " год не является високосным"); //доработать: не выводит каждый 400ый год согласно условию
+            if (year % 4 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " год является високосным");
+                } else if (everyHoundredYear) {
+                    System.out.println(year + " год не является високосным");
+                }
+            }
+
+            //задача 4
+            int deliveryDistance = 101;
+            int numberOfDayOne = 1;
+            int numberOfDayTwo = 2;
+            int numberOfDayThree = 3;
+
+            if (deliveryDistance < 20) {
+                System.out.println("Потребуется дней: " + numberOfDayOne);
+            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                System.out.println("Потребуется дней: " + numberOfDayTwo);
+            } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+                System.out.println("Потребуется дней: " + numberOfDayThree);
+            } else {
+                System.out.println("Мы не доставляем в эту зону");
+            }
+
+
+            //задача 5
+            int monthNumber = 1;
+
+            switch (monthNumber) {
+                case 1:
+                case 2:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+                case 12:
+                    System.out.println("Зима");
+                    break;
+                default:
+                    System.out.println("Такого времени года ещё не придумали");
+            }
         }
-
-        //задача 4
-        int deliveryDistance = 101;
-
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + 1);
-        }  else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: " + 2);
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: " + 3);
-        } else {
-            System.out.println("Мы не доставляем в эту зону");
-        }
-
-        //задача 5
-        int monthNumber = 1;
-
-        switch (monthNumber) {
-            case 1:
-            case 2:
-                System.out.println("Зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
-                break;
-            default:
-                System.out.println("Такого времени года ещё не придумали");
-        }
-    }
-
-
     }
